be4f6565080468ce8882e7def048e08762a3d2e5
Foreinyel <Foreinyel@163.com>
2022-06-27 22:42:21 +0800

    search button

--- src/Example.tsx
+++ src/Example.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import Component from "./Component";
 
 export const Example = () => {
-  return <Component />;
+  return (
+    <div>
+      <Component
+        onClick={() => {
+          alert("search button clicked");
+        }}
+      >
+        Search Button
+      </Component>
+    </div>
+  );
 };
 
 export default Example;
